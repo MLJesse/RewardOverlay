ab311fc32f25e586bc62ece991be88324af5a727
MLJesse <m_j_e_s_s_e@live.com>
2020-06-12 23:23:55 -0400

    Removed local user

--- script/install.bat
+++ script/install.bat
@@ -1,4 +1,4 @@
-set path=%path%+;C:\Users\m_j_e\AppData\Local\Programs\Python\Python38\Scripts;C:\Users\m_j_e\AppData\Local\Programs\Python\Python38
+set path=%path%+;C:\Users\PUT YOUR USER HERE\AppData\Local\Programs\Python\Python38\Scripts;C:\Users\PUT YOUR USER HERE\AppData\Local\Programs\Python\Python38
 
 pip install selenium
 
